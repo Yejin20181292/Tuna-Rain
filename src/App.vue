797a8e3fcedac24eb82883d1dc46a2391b8b1272
Yeji 20181292 <se84838483@gmail.com>
2026-07-10 08:55:49 +0900

    first upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="fullscreen-bg"></div>
+</template>
+
+<style scoped>
+.fullscreen-bg {
+  width: 100vw;
+  height: 100vh;
+  background-image: url('/factory-bg.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
